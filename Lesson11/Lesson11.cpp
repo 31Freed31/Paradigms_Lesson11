c0b27a0816829647f4b0238e3e269ab34c046d66
Freed31 <abdulganievartem3103@gmail.com>
2024-10-04 22:53:45 +0200

    Добавьте файлы проекта.

---
+++ Lesson11/Lesson11.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <vector>
+
+class Talent {
+public:
+    virtual void showTalent() const = 0;
+    virtual ~Talent() {}
+};
+
+class Dog {
+    std::string name;
+    std::vector<Talent*> talents;
+
+public:
+    Dog(std::string dogName) : name(dogName) {}
+
+    void addTalent(Talent* talent) {
+        talents.push_back(talent);
+    }
+
+    void show_talents() const {
+        std::cout << "This is " << name << " and it has some talents:\n";
+        for (const auto& talent : talents) {
+            talent->showTalent();
+        }
+    }
+
+    ~Dog() {
+        for (Talent* talent : talents) {
+            delete talent;
+        }
+    }
+};
+
+class Swimming : public Talent {
+public:
+    void showTalent() const override {
+        std::cout << "It can \"Swim\"\n";
+    }
+};
+
+class Dancing : public Talent {
+public:
+    void showTalent() const override {
+        std::cout << "It can \"Dance\"\n";
+    }
+};
+
+class Counting : public Talent {
+public:
+    void showTalent() const override {
+        std::cout << "It can \"Count\"\n";
+    }
+};
+
+int main() {
+    Dog dog1("Sharik");
+
+    dog1.addTalent(new Swimming());
+    dog1.addTalent(new Dancing());
+    dog1.addTalent(new Counting());
+
+    dog1.show_talents();
+
+    return 0;
+}
